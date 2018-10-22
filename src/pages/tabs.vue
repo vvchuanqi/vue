09d444c24dbe5553109b186d67e168d494faf60b
vvchuanqi <vvchuanqi@163.com>
2018-10-22 20:31:37 +0800

    新增了一些文件图标

--- src/pages/tabs.vue
+++ src/pages/tabs.vue
@@ -1,83 +1,83 @@
 <template>
 <div class="hello">
-  <header>
-    <div class="title">{{title}}</div>
-  </header>
-  <div class="tab-panel">
-    <transition :name="transitionName">
-      <router-view></router-view>
-    </transition>
-  </div>
-  <footer>
-    <div class="tabbar">
-      <div class="tabbar-item" :class="{on:currentIndex === 0}" v-on:click="tabbarClick(0)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_home.svg" height="28" width="28"></i><span class="tabbar-text">首页</span></div>
-      <div class="tabbar-item" :class="{on:currentIndex === 1}" v-on:click="tabbarClick(1)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_home.svg" height="28" width="28"></i><span class="tabbar-text">场景</span></div>
-      <div class="tabbar-item" :class="{on:currentIndex === 2}" v-on:click="tabbarClick(2)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_home.svg" height="28" width="28"></i><span class="tabbar-text">场景</span></div>
-      <div class="tabbar-item" :class="{on:currentIndex === 3}" v-on:click="tabbarClick(3)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_me.svg" height="28" width="28"></i><span class="tabbar-text">我</span></div>
-    </div>
-  </footer>
+	<header>
+		<div class="title">{{title}}</div>
+	</header>
+	<div class="tab-panel">
+		<transition :name="transitionName">
+			<router-view></router-view>
+		</transition>
+	</div>
+	<footer>
+		<div class="tabbar">
+			<div class="tabbar-item" :class="{on:currentIndex === 0}" v-on:click="tabbarClick(0)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_home.svg" height="28" width="28"></i><span class="tabbar-text">首页</span></div>
+			<div class="tabbar-item" :class="{on:currentIndex === 1}" v-on:click="tabbarClick(1)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_pj1.svg" height="28" width="28"></i><span class="tabbar-text">场景</span></div>
+			<div class="tabbar-item" :class="{on:currentIndex === 2}" v-on:click="tabbarClick(2)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_pj2.svg" height="28" width="28"></i><span class="tabbar-text">场景</span></div>
+			<div class="tabbar-item" :class="{on:currentIndex === 3}" v-on:click="tabbarClick(3)"><i class="tabbar-icon"><img src="../assets/icon_tabbar_me.svg" height="28" width="28"></i><span class="tabbar-text">我</span></div>
+		</div>
+	</footer>
 
 </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
-  data() {
-    return {
-      title: '管理后台',
-      currentIndex: 0,
-      transitionName: "fade",
-    }
-  },
-  mounted() {
-    let to = this.$route.name;
-    console.log("rout to :" + to);
-    if (to == "home") {
-      this.currentIndex = 0;
-      this.title = "首页";
-    } else if (to == "stage1") {
-      this.currentIndex = 1;
-      this.title = "场景1";
-    } else if (to == "stage2") {
-      this.currentIndex = 2;
-      this.title = "场景2";
-    } else if (to == "my") {
-      this.currentIndex = 3;
-      this.title = "我的";
-    }
-  },
-  methods: {
-    tabbarClick(index) {
-      this.currentIndex = index;
-      switch (this.currentIndex) {
-        case 0:
-          this.$router.replace({
-            path: "/tabs/home"
-          });
-          this.title = "首页";
-          break;
-        case 1:
-          this.$router.replace({
-            path: "/tabs/stage1"
-          });
-          this.title = "场景1";
-          break;
-        case 2:
-          this.$router.replace({
-            path: "/tabs/stage2"
-          });
-          this.title = "场景2";
-          break;
-        case 3:
-          this.$router.replace({
-            path: "/tabs/my"
-          });
-          this.title = "我的";
-          break;
-      }
-    }
-  }
+	name: 'home',
+	data() {
+		return {
+			title: '管理后台',
+			currentIndex: 0,
+			transitionName: "fade",
+		}
+	},
+	mounted() {
+		let to = this.$route.name;
+		console.log("rout to :" + to);
+		if (to == "home") {
+			this.currentIndex = 0;
+			this.title = "首页";
+		} else if (to == "stage1") {
+			this.currentIndex = 1;
+			this.title = "场景1";
+		} else if (to == "stage2") {
+			this.currentIndex = 2;
+			this.title = "场景2";
+		} else if (to == "my") {
+			this.currentIndex = 3;
+			this.title = "我的";
+		}
+	},
+	methods: {
+		tabbarClick(index) {
+			this.currentIndex = index;
+			switch (this.currentIndex) {
+			case 0:
+				this.$router.replace({
+					path: "/tabs/home"
+				});
+				this.title = "首页";
+				break;
+			case 1:
+				this.$router.replace({
+					path: "/tabs/stage1"
+				});
+				this.title = "场景1";
+				break;
+			case 2:
+				this.$router.replace({
+					path: "/tabs/stage2"
+				});
+				this.title = "场景2";
+				break;
+			case 3:
+				this.$router.replace({
+					path: "/tabs/my"
+				});
+				this.title = "我的";
+				break;
+			}
+		}
+	}
 }
 </script>
 <style scoped lang='less'>
